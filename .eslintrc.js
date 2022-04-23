5132716a66d38cff4dcc77364f52ac5d7fc19fc4
Jiwoong Jung <kanziwoong@gmail.com>
2022-04-23 17:18:18 +0900

    Add Own Eslint Rule (#2)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,12 @@ module.exports = {
     node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'standard',
+    'kanziw',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-    'comma-dangle': ['error', 'always-multiline'],
   },
 }
