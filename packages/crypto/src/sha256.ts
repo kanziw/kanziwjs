9dbdf96505d9a8fe07751731dcd84616e6d20d40
Jiwoong Jung <kanziwoong@gmail.com>
2022-10-01 20:10:28 +0900

    Add publishConfig in each package.json

--- packages/crypto/src/sha256.ts
+++ packages/crypto/src/sha256.ts
@@ -1,3 +1,5 @@
 import crypto from 'crypto'
 
-export const sha256 = (str: string) => crypto.createHash('sha256').update(str).digest()
+export const sha256 = (str: string): Buffer => (
+  crypto.createHash('sha256').update(str).digest()
+)
